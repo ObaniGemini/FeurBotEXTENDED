7a0f297cee820a661dfd3fe7c20ad850694b63af
ObaniGemini <robin@pi-et-ro.net>
2023-05-09 00:20:00 +0200

    stuff

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import discord
 import random
 
-answers = {
-    "quoi": ["feur", "feur.", "feur!", "Feur.", "FEUR", "FEUR!!", "coubeh", "COUBEH", "QUOICOUBEH", ";)", "APANIAN"],
-    "qui": ["quette", "ket", "KET", "QUETTE", "KI", "kou :3", "koulol", "Le ch'i (chinois simplifié : 气 ; chinois traditionnel : 氣 ; pinyin : qì ; Wade : ch'i ; EFEO : ts'i), ou ki (japonais : 気), ou encore chi, que l'on peut traduire par « flux d'énergie naturelle », est une notion des cultures chinoise et japonaise qui désigne un principe fondamental formant et animant l'univers et la vie"],
-    "comment": ["DANT COUSTEAU", "DANTE CHE GUEVARA"],
-    "hein": ["2", "deux", "DEUX", "deux?", "DEUX!", "deux x)", "deux xD", "deux ^^", "deux! ;)"],
-    "oui": ["STITI", "stiti", "STITI!!!","STICRAM", "LLY WONKA", "lly wonka x)", "lly wonka", "STITI xD"],
-    "nan": ["si", "cy", "CY", "NANCY"],
-    "non": ["bril", "BRIL", "BRIL X)", "BRIL!!"],
-    "allo": ["à l'huile", "À L'HUILE", "A L'HUILE!!!!", "a l'huile ^_^", "alohomora", "ALOHOMORA"],
-    "chaud": ["CHAUSSURE", "CHAUSSETTE!", "CHAUSSETTE x)", "chocolat", "CHOCOLAT", "CHAUD CHAUD CHOCOLAT!", "CHAAUD DEVANT!", "CHAUD DEVANT x)", "CHAUDS LES MARRONS CHAUDS!", "CHAUUDS LES MARRONS CHAUDS x))", "CHAUD CACAO", "MAGE :D", "AH x)", "biz"],
-    "what": ["feur", "WATI B", "WATTOUAT", "fewr", "wati b!", "wattouat", "wati style", "wati shirt"],
-    "quoicoubeh": ["quoicouflop", "quoicounul", "quoicou tg", "quoicoustyle", "quoicoupatroopa", "quoicoubecousseh", "quoicoubecoussecoudeh", "...", "tg", "lol", "tu m'as bien eu xd", "bien joué!!"]
-}
+check = [
+    ["quoi", "koi"],
+    ["qui", "ki"],
+    ["comment"],
+    ["hein", "un", " 1", "1 "],
+    ["oui"],
+    ["nan"],
+    ["non"],
+    ["allo"],
+    ["chaud", "chau", "chauds"],
+    ["what", "wat"],
+    ["quoicoubeh", "quoicoub", "quoicoube"]
+]
+
+answers = [
+    ["feur", "feur.", "feur!", "Feur.", "FEUR", "FEUR!!", "coubeh", "COUBEH", "QUOICOUBEH", ";)", "APANIAN"],
+    ["quette", "ket", "KET", "QUETTE", "KI", "kou :3", "koulol", "Le ch'i (chinois simplifié : 气 ; chinois traditionnel : 氣 ; pinyin : qì ; Wade : ch'i ; EFEO : ts'i), ou ki (japonais : 気), ou encore chi, que l'on peut traduire par « flux d'énergie naturelle », est une notion des cultures chinoise et japonaise qui désigne un principe fondamental formant et animant l'univers et la vie"],
+    ["DANT COUSTEAU", "DANTE CHE GUEVARA"],
+    ["2", "deux", "DEUX", "deux?", "DEUX!", "deux x)", "deux xD", "deux ^^", "deux! ;)"],
+    ["STITI", "stiti", "STITI!!!","STICRAM", "LLY WONKA", "lly wonka x)", "lly wonka", "STITI xD"],
+    ["si", "cy", "CY", "NANCY"],
+    ["bril", "BRIL", "BRIL X)", "BRIL!!"],
+    ["à l'huile", "À L'HUILE", "A L'HUILE!!!!", "a l'huile ^_^", "alohomora", "ALOHOMORA"],
+    ["CHAUSSURE", "CHAUSSETTE!", "CHAUSSETTE x)", "chocolat", "CHOCOLAT", "CHAUD CHAUD CHOCOLAT!", "CHAAUD DEVANT!", "CHAUD DEVANT x)", "CHAUDS LES MARRONS CHAUDS!", "CHAUUDS LES MARRONS CHAUDS x))", "CHAUD CACAO", "MAGE :D", "AH x)", "biz"],
+    ["feur", "WATI B", "WATTOUAT", "fewr", "wati b!", "wattouat", "wati style", "wati shirt"],
+    ["quoicouflop", "quoicounul", "quoicou tg", "quoicoustyle", "quoicoupatroopa", "quoicoubecousseh", "quoicoubecoussecoudeh", "...", "tg", "lol", "tu m'as bien eu xd", "bien joué!!"]
+]
 
 taslescramptes = ["hein?", "hein...", "quoi?", "quoi??", "QUOI", "QUOI? xd", "comment ?", "plait-il ?", "Plait-il ?", "pardon ?", "Pardon ?", "...", "oui", "non", "et toi?", "^^"]
 
-remove = [".", "!", "?", " ", "`", "\n", "\t", "*", "`", "'"]
+remove = [".", "!", "?", "`", "\n", "*", "`", "'"]
 
 
 class MyClient(discord.Client):
@@ -28,11 +42,15 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
+
         s = message.content.lower();
         s = s.replace("é", "e")
         s = s.replace("è", "e")
         s = s.replace("ê", "e")
         s = s.replace("ë", "e")
+        
+        if message.content == "1":
+            await message.channel.send(answers[3][random.randint(0, len(answers[3]) - 1)])
 
         for r in remove:
             s = s.replace(r, "");
@@ -40,10 +58,11 @@ class MyClient(discord.Client):
         if "jeudemerde" in s or "jeuxdemerde" in s:
             await message.channel.send("https://cdn.discordapp.com/attachments/759836645146755076/1103796462267158588/cover.jpg")
         
-        for key in answers.keys():
-            if s.endswith(key):
-                await message.channel.send(answers[key][random.randint(0, len(answers[key]) - 1)])
-                return
+        for i in range(len(check)):
+            for key in check[i]:
+                if s.endswith(key):
+                    await message.channel.send(answers[i][random.randint(0, len(answers[i]) - 1)])
+                    return
 
         s = s.replace("s", "");
         if "lecrampte" in s or "lecrante" in s or "lecramte" in s:
